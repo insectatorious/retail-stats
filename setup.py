c8d854aa1827e45bd914ae52b17ebc6e09b77fbd
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2020-09-17 16:45:18 +0100

    Add link to repo in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
   description="A simple way to calculate retail stats",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/pypa/sampleproject",
+  url="https://github.com/insectatorious/retail-stats",
   packages=setuptools.find_packages(),
   classifiers=[
     "Programming Language :: Python :: 3",
