0ec2819266a983cec78094c3d77859dab5febdd5
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2020-09-18 09:15:20 +0100

    Explicitly call out Python versions
    Instead of saying Python 3, the setup script now
    explicitly mentions 3.7, 3.8 and 3.9.

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setuptools.setup(
   url="https://github.com/insectatorious/retail-stats",
   packages=setuptools.find_packages(),
   classifiers=[
-    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9"
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
     "Development Status :: 3 - Alpha",
