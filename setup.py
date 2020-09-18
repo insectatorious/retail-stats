7e4cbb094401081ac47417193a3f4df780a5f424
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2020-09-18 09:25:54 +0100

    Fix typo in PyPi classifiers tag

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
   classifiers=[
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9"
+    "Programming Language :: Python :: 3.9",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
     "Development Status :: 3 - Alpha",
